c9eb7a324c3d1168abadef85aeb5366cad2f36e5
hideki74 <07hidek04@gmail.com>
2024-05-29 16:05:10 +0900

    タグ機能追加

--- resources/js/components/ArticleTagsInput.vue
+++ resources/js/components/ArticleTagsInput.vue
@@ -1,36 +1,38 @@
 <template>
     <div>
+      <input type="hidden" name="tags" :value="tagsJson">
       <vue-tags-input
         v-model="tag"
         :tags="tags"
+        placeholder="タグを5個ままで入力できます"
         :autocomplete-items="filteredItems"
+        :add-on-key="[13, 32]"
         @tags-changed="newTags => tags = newTags"
       />
     </div>
-  </template>
+</template>
   
-  <script>
+<script>
   import VueTagsInput from '@johmun/vue-tags-input';
   
   export default {
     components: {
       VueTagsInput,
     },
+    props: {
+        initialTags: {
+          type: Array,
+          default: [],
+        },
+        autocompleteItems: {
+          type: Array,
+          default: [],
+        },
+    },
     data() {
       return {
         tag: '',
-        tags: [],
-        autocompleteItems: [{
-          text: 'Spain',
-        }, {
-          text: 'France',
-        }, {
-          text: 'USA',
-        }, {
-          text: 'Germany',
-        }, {
-          text: 'China',
-        }],
+        tags: this.initialTags,
       };
     },
     computed: {
@@ -39,9 +41,13 @@
           return i.text.toLowerCase().indexOf(this.tag.toLowerCase()) !== -1;
         });
       },
+      tagsJson() {
+        return JSON.stringify(this.tags);
+      }
     },
   };
   </script>
+
   <style lang="css" scoped>
     .vue-tags-input {
       max-width: inherit;
@@ -55,6 +61,9 @@
       margin-right: 4px;
       border-radius: 0px;
       font-size: 13px;
+    }
+    .vue-tags-input .ti-tag::before {
+        content: "#";
     }
   </style>
   
